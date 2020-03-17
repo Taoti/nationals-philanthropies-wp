c6b3e9d1d57a469fa8d2447921f7390e87933cc8
James Pfleiderer <jpfleiderer@gmail.com>
2020-03-17 11:27:44 -0400

    clip path figured out for image and color overlay

--- web/wp-content/themes/nats-philanthropies-theme/js/development/after-libs/lazyload-manual.js
+++ web/wp-content/themes/nats-philanthropies-theme/js/development/after-libs/lazyload-manual.js
@@ -1,7 +1,15 @@
 // Use lazysizes to get lazy loading on css background images.
 document.addEventListener('lazybeforeunveil', function(e){
-    var bg = e.target.getAttribute('data-bg');
-    if(bg){
-        e.target.style.backgroundImage = 'url(' + bg + ')';
-    }
+
+  var bg = e.target.getAttribute('data-bg');
+  if( bg ){
+    e.target.style.backgroundImage = 'url(' + bg + ')';
+  }
+
+  // This is for the <image> elements inside of <svg>s.
+  var xlink_href = e.target.getAttribute('data-xlink:href');
+  if( xlink_href ){
+    e.target.setAttribute( 'xlink:href', xlink_href );
+  }
+
 });
--- web/wp-content/themes/nats-philanthropies-theme/js/scripts.js
+++ web/wp-content/themes/nats-philanthropies-theme/js/scripts.js
@@ -2531,10 +2531,18 @@ function taoti_update_header_height_property(){
 
 // Use lazysizes to get lazy loading on css background images.
 document.addEventListener('lazybeforeunveil', function(e){
-    var bg = e.target.getAttribute('data-bg');
-    if(bg){
-        e.target.style.backgroundImage = 'url(' + bg + ')';
-    }
+
+  var bg = e.target.getAttribute('data-bg');
+  if( bg ){
+    e.target.style.backgroundImage = 'url(' + bg + ')';
+  }
+
+  // This is for the <image> elements inside of <svg>s.
+  var xlink_href = e.target.getAttribute('data-xlink:href');
+  if( xlink_href ){
+    e.target.setAttribute( 'xlink:href', xlink_href );
+  }
+
 });
 
 // Use this to load fonts from Google Fonts, Typekit, Fonts.com, and Fontdeck, as well as self-hosted web fonts
