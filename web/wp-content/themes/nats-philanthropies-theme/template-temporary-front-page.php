a339cf4208ea540d103fd2f0b483b9a83d6128e1
James Pfleiderer <jpfleiderer@gmail.com>
2020-03-17 15:30:51 -0400

    responsive image sort of

--- web/wp-content/themes/nats-philanthropies-theme/template-temporary-front-page.php
+++ web/wp-content/themes/nats-philanthropies-theme/template-temporary-front-page.php
@@ -12,7 +12,7 @@ get_header();
 <div class="hero">
 
   <div class="hero-bg">
-    <?php include get_stylesheet_directory().'/images/bg-home-hero2.php'; ?>
+    <?php include get_stylesheet_directory().'/images/bg-home-hero3.php'; ?>
   </div>
 
   <div class="hero-inner l-container">
