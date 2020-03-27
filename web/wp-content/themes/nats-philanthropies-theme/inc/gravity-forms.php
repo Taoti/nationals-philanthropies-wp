42fc6fb17d1f9dd8a3d241372798829c5f920f80
James Pfleiderer <jpfleiderer@gmail.com>
2020-03-27 11:43:11 -0400

    update function name

--- web/wp-content/themes/nats-philanthropies-theme/inc/gravity-forms.php
+++ web/wp-content/themes/nats-philanthropies-theme/inc/gravity-forms.php
@@ -7,7 +7,7 @@
 * RETURNS : string - the possibly modified HTML string for the submit button
 *   NOTES : https://docs.gravityforms.com/gform_submit_button/
 */
-function form_submit_button( $button, $form ) {
+function taoti_edit_gf_submit_button( $button, $form ) {
 		return '<button class="button gform_button" id="gform_submit_button_'.$form['id'].'">Submit<i class="menu-icon">'.file_get_contents(get_stylesheet_directory() . '/images/icon-arrow.svg').'</i></button>';
 }
-add_filter( 'gform_submit_button', 'form_submit_button', 10, 2 );
+add_filter( 'gform_submit_button', 'taoti_edit_gf_submit_button', 10, 2 );
