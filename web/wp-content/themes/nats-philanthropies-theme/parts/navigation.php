d481e68e899aca123c7e17f98551c5063815571d
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-08 17:37:15 -0400

    sub menus WIP

--- web/wp-content/themes/nats-philanthropies-theme/parts/navigation.php
+++ web/wp-content/themes/nats-philanthropies-theme/parts/navigation.php
@@ -5,10 +5,27 @@ $menuLocations = get_nav_menu_locations();
 // echo "<pre>"; print_r( $menuLocations ); echo "</pre>";
 if( isset($menuLocations[$theme_location]) ):
 	$main_nav = wp_get_nav_menu_items($menuLocations[$theme_location]);
-	// echo "<pre>"; var_dump($main_nav); echo "</pre>";
+	// echo "<pre>"; print_r($main_nav); echo "</pre>";
 
-	$nav_item_counter = 1;
-	if( !empty($main_nav) ): ?>
+	if( !empty($main_nav) ):
+
+		$top_level_nav = [];
+		$sub_navs = [];
+
+		foreach( $main_nav as $nav_item ){
+			$parent_ID = $nav_item->menu_item_parent;
+
+			if( $nav_item->menu_item_parent === '0' ){
+				$top_level_nav[] = $nav_item;
+
+			} else {
+				$sub_navs[$parent_ID][] = $nav_item;
+			}
+
+		}
+		// echo "<pre>"; print_r($sub_navs); echo "</pre>";
+
+	?>
 
 <nav class="navContainer navContainer-mobile">
 	<ul class="menu menu-mobileMenu">
@@ -27,8 +44,8 @@ if( isset($menuLocations[$theme_location]) ):
 
 		</li>
 
-		<li class="menu-item menu-item-mobileMenu">
-			<a href="https://www.mlb.com/nationals/forms/give" class="menu-link menu-link-mobileMenu">
+		<li class="menu-item menu-item-mobileMenu<?php if( $is_last_item ): ?> menu-item-bigButton<?php endif; ?>">
+			<a href="https://www.mlb.com/nationals/forms/give" class="menu-link menu-link-mobileMenu menu-link-bigButton">
 				Donate
 				<i class="menu-icon"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon-arrow.svg'); ?></i>
 			</a>
@@ -39,20 +56,47 @@ if( isset($menuLocations[$theme_location]) ):
 
 <nav class="navContainer navContainer-<?php echo $theme_location; ?>">
 	<ul class="menu menu-mainMenu">
-		<?php foreach( $main_nav as $nav_item ): ?>
+		<?php $nav_item_counter = 1; ?>
+		<?php foreach( $top_level_nav as $nav_item ): ?>
 			<?php
-			// echo "<pre>"; print_r($nav_item); echo "</pre>";
+			$nav_item_ID = $nav_item->ID;
 			$title = $nav_item->title;
 			$href = $nav_item->url;
+			$is_last_item = ( $nav_item_counter === count($main_nav) );
 			?>
 
-			<li class="menu-item menu-item-mainMenu">
-				<a href="<?php echo $href; ?>" class="menu-link menu-link-mainMenu">
+			<li class="menu-item menu-item-mainMenu<?php if( $is_last_item ): ?> menu-item-bigButton<?php endif; ?>">
+				<a href="<?php echo $href; ?>" class="menu-link menu-link-mainMenu<?php if( $is_last_item ): ?> menu-link-bigButton<?php endif; ?>">
 					<?php echo $title; ?>
-					<?php if( $nav_item_counter === count($main_nav) ): ?>
+					<?php if( $is_last_item ): ?>
 					<i class="menu-icon"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon-arrow.svg'); ?></i>
 					<?php endif; ?>
 				</a>
+
+				<?php if( isset($sub_navs[$nav_item_ID]) && !empty($sub_navs[$nav_item_ID]) ): ?>
+				<div class="subMenu">
+					<div class="subMenu-inner">
+
+						<div class="subMenu-titleArea">
+
+							<a class="submenu-titleAreaLink" href="<?php echo $href; ?>"><?php echo $title; ?></a>
+
+							<p class="subMenu-titleAreaDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+						</div>
+
+						<ul class="subMenu-menu">
+					<?php foreach( $sub_navs[$nav_item_ID] as $sub_nav_item ): ?>
+							<li class="subMenu-menu-item">
+								<a class="subMenu-menu-link" href="<?php $sub_nav_item->href; ?>"><?php echo $sub_nav_item->title; ?></a>
+							</li>
+					<?php endforeach; ?>
+						</ul>
+
+					</div>
+				</div>
+				<?php endif; ?>
+
 			</li>
 
 			<?php $nav_item_counter++; ?>
