af53566bf3cf6696bef79b673343e6020d06d184
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-13 12:23:43 -0400

    fix some page builder options

--- web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/featured_event.php
+++ web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/featured_event.php
@@ -1,8 +1,8 @@
 <?php
-use Modules\FeaturedEvent;
+// use Modules\FeaturedEvent;
 
-$args = array(
-	'selected_event' => get_sub_field('selected_event'),
-);
-$featuredEvent = new FeaturedEvent($args);
-$featuredEvent->render();
+// $args = array(
+// 	'selected_event' => get_sub_field('selected_event'),
+// );
+// $featuredEvent = new FeaturedEvent($args);
+// $featuredEvent->render();
