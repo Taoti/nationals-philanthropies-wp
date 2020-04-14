4c3f30176d8e44e8891b74f99b21391fa7b21dd5
Chris <chodges@taoti.com>
2020-04-14 12:12:18 -0400

    updates to hero on homepage

--- web/wp-content/themes/nats-philanthropies-theme/front-page.php
+++ web/wp-content/themes/nats-philanthropies-theme/front-page.php
@@ -1,5 +1,5 @@
 <?php
-// use Modules\CTA;
+use Modules\Hero;
 
 $temporary_page_enabled = ( get_field( 'temporary_landing_page_is_enabled', 'option' ) );
 
@@ -16,7 +16,18 @@ taoti_enqueue_critical_css( get_template_directory().'/styles/css/critical/front
 get_header();
 ?>
 
-
+<?php
+$args = [
+  'heading_line_1' => 'One Pursuit For',
+  'heading_line_2' => 'A Better Washington',
+  'description' => 'We are committed to holistically improving the lives of children and families across Washington, D.C. and beyond. Join our movement today.',
+  'button_label' => 'make a donation',
+  'button_link' => '#',
+  'background_image_url' => wp_get_attachment_image_src( get_post_thumbnail_id( get_the_id()), 'large')
+];
+$hero = new Hero($args);
+$hero->render();
+?>
 
 <div class="content">
   <div class="content-inner l-container">
