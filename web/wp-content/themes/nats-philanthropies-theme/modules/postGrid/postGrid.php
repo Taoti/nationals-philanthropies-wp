bcf1b1bae823ebed0388488ef39c570cf0fa152c
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-16 15:58:12 -0400

    add page tear, contentGroup to postGrid

--- web/wp-content/themes/nats-philanthropies-theme/modules/postGrid/postGrid.php
+++ web/wp-content/themes/nats-philanthropies-theme/modules/postGrid/postGrid.php
@@ -33,13 +33,24 @@ class PostGrid {
 
 		extract(array_merge($this->defaults, $args));
 
+		$args = [
+			'primary_heading' => $primary_heading_line_1,
+			'secondary_heading' => $primary_heading_line_2,
+			'description' => $description,
+			'cta_link' => $button_url,
+			'cta_label' => $button_label,
+		];
+		$content_group = new ContentGroup($args);
+		$header_html = $content_group->compile();
+
 		$this->context = Timber::get_context();
+		$this->context['header_html'] = $header_html;
+		$this->context['grid_items'] = $grid_items;
 		$this->context['primary_heading_line_1'] = $primary_heading_line_1;
 		$this->context['primary_heading_line_2'] = $primary_heading_line_2;
 		$this->context['description'] = $description;
 		$this->context['button_url'] = $button_url;
 		$this->context['button_label'] = $button_label;
-		$this->context['grid_items'] = $grid_items;
 		$this->context['classes'] = implode(' ', $classes);
 
 	}
