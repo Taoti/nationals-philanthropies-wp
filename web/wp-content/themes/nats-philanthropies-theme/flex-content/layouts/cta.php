b9b64ac744482549b603f327c62e5636b1402493
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-15 22:45:58 -0400

    CTA module set up

--- web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/cta.php
+++ web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/cta.php
@@ -4,6 +4,10 @@ use Modules\CTA;
 $args = array(
 	'heading' => get_sub_field('heading'),
 	'description' => get_sub_field('description'),
+	'button_label' => get_sub_field('button_label'),
+	'button_url' => get_sub_field('button_url'),
+	'background_image' => get_sub_field('background_image'),
+	'overlay_color' => get_sub_field('overlay_color'),
 );
 $cta = new CTA($args);
 $cta->render();
