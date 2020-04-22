1096907c0faca94592061886adaba94d363ed4d9
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-22 12:19:33 -0400

    featuredEvent and listingItem modules

--- web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/featured_event.php
+++ web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/featured_event.php
@@ -1,8 +1,27 @@
 <?php
-// use Modules\FeaturedEvent;
+use Modules\ListingItem;
+use JP\Get;
 
-// $args = array(
-// 	'selected_event' => get_sub_field('selected_event'),
-// );
-// $featuredEvent = new FeaturedEvent($args);
-// $featuredEvent->render();
+$selected_event = get_sub_field('selected_event');
+
+if( is_a( $selected_event, 'WP_Post') ){
+
+	$listingItem_args = [
+		'primary_heading' => get_the_title( $selected_event ),
+		'subtitle' => get_sub_field( 'subtitle' ),
+		'excerpt' => get_the_excerpt( $selected_event ),
+		'permalink' => get_permalink( $selected_event ),
+		'primary_button_url' => get_field( 'rsvp_url', $selected_event->ID ),
+		'primary_button_label' => 'RSVP',
+		'secondary_button_url' => get_field( 'sponsor_url', $selected_event->ID ),
+		'secondary_button_label' => get_field( 'sponsor_name', $selected_event->ID ),
+		'image_array' => Get::featured_image_array( $selected_event->ID ),
+		'classes' => [
+			'l-module',
+			'featuredEvent',
+		],
+	];
+	$listingItem = new ListingItem( $listingItem_args );
+	$listingItem->render();
+
+}
