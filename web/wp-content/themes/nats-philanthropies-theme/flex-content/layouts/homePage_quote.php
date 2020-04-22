71937f9e427f321acbf21fb6f09def84943012bb
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-22 13:18:23 -0400

    split quote into distinct home and non-home

--- web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/homePage_quote.php
+++ web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/homePage_quote.php
@@ -1,10 +1,11 @@
 <?php
 use Modules\ContentGroup;
-use Modules\Quote;
+use JP\Get;
 ?>
 <section class="l-homePageModule home-quote scrollspy scrollspy-light">
   <div class="home-module-inner">
     <div class="home-content-row">
+
       <div class="home-content-column">
         <?php
         // ContentGroup
@@ -19,19 +20,59 @@ use Modules\Quote;
         $contentGroup->render();
         ?>
       </div>
+
       <div class="home-content-column">
         <?php
         // Quote
-        $args = [
-         'image' => get_sub_field('image'),
-         'description' => get_sub_field('quoted_text'),
-         'author' => get_sub_field('attribution_name'),
-         'location' => get_sub_field('attribution_description'),
-        ];
-        $quote = new Quote($args);
-        $quote->render();
+        $image = get_sub_field('image');
+        $quoted_text = get_sub_field('quoted_text');
+        $author = get_sub_field('attribution_name');
+        $location = get_sub_field('attribution_description');
+
+        $image_html = '';
+        if( is_array($image) ){
+          $image_args = [
+            'image_array' => $image,
+            'size' => 'medium',
+            'classes' => ['homeQuote-image'],
+          ];
+          $image_html = Get::image_html( $image_args );
+        }
         ?>
+
+        <div class="homeQuote">
+          <div class="homeQuote-row">
+
+            <?php if( $image_html ): ?>
+            <div class="homeQuote-column homeQuote-column-image">
+              <div class="homeQuote-imageContainer"><?php echo $image_html; ?></div>
+            </div>
+            <?php endif; ?>
+
+            <?php if( $quoted_text ) : ?>
+            <div class="homeQuote-column homeQuote-column-text">
+
+              <span class="homeQuote-icon">&ldquo;</span>
+
+              <div class="homeQuote-quoteText">
+                <?php echo $quoted_text; ?>
+              </div>
+
+              <?php if( $author ) : ?>
+              <div class="homeQuote-author"><?php echo $author; ?></div>
+              <?php endif; ?>
+
+              <?php if( $location ) : ?>
+              <div class="homeQuote-location"><?php echo $location; ?></div>
+              <?php endif; ?>
+
+            </div>
+            <?php endif; ?>
+
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 </section>
