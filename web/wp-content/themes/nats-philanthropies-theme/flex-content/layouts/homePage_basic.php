c87ab971ab08c9bf31add69ca63490b531153ad4
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-27 13:44:26 -0400

    remove basic homepage builder module

---
+++ web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/homePage_basic.php
@@ -0,0 +1,2 @@
+<?php
+// The intention of this layout was to load a homepage section with JUST the common fields - heading, description, button label, button url. Basically a homepage section without the quote, image, or stats. Dropped due to dev time contraints for launch.
