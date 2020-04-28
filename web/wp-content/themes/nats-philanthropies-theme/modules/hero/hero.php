b9b3f247461d44a15d15fe5410d167dab714d9a2
James Pfleiderer <jpfleiderer@gmail.com>
2020-04-28 11:49:18 -0400

    add documentation

--- web/wp-content/themes/nats-philanthropies-theme/modules/hero/hero.php
+++ web/wp-content/themes/nats-philanthropies-theme/modules/hero/hero.php
@@ -19,42 +19,39 @@ class Hero {
 
 	public function __construct( $args=[] ){
 
+		// Save a boolean as to whether or not this should use the home page twig template. This of course depends on `is_front_page()`, and also on the use of the temporary landing page (which has its own hero).
 		$this->use_home_template = is_front_page() && !get_field( 'temporary_landing_page_is_enabled', 'option' );
 
+		// Set up the default <img> html for the "paint splatter" accent image.
 		$accent_image_1_html_default = '<img class="hero-accent hero-accent-1 lazyload" data-srcset="' . get_stylesheet_directory_uri() . '/images/accent-spatter-1.png 1x, ' . get_stylesheet_directory_uri() . '/images/accent-spatter-1@2x.png 2x" width="477" height="82" alt="Paint splatter accent">';
-		$accent_image_2_html_default = '<img class="hero-accent hero-accent-2 lazyload" data-srcset="' . get_stylesheet_directory_uri() . '/images/accent-spatter-2.png 1x, ' . get_stylesheet_directory_uri() . '/images/accent-spatter-2@2x.png 2x" width="690" height="67" alt="Paint splatter accent">';
 
-		$post_title = get_the_title();
-		$post_title_array = explode( ' ', $post_title );
-		// echo "<pre>"; var_dump($post_title_array); echo "</pre>";
+		// Set up defaults for the heading lines 1 and 2. This will take the post title, split it into an array of words, and split that array in half. Then each half of the post title makes the heading lines 1 and 2.
 		$heading_line_1 = false;
 		$heading_line_2 = false;
 
-		if( $post_title && count($post_title_array) > 1 ){
+		$post_title = get_the_title();
+		$post_title_array = explode( ' ', $post_title );
 
+		if( $post_title && count($post_title_array) > 1 ){
 			$post_title_divided_to_lines = array_chunk( $post_title_array, (count($post_title_array) / 2) );
-			// echo "<pre>"; var_dump($post_title_divided_to_lines); echo "</pre>";
 			$heading_line_1 = implode( ' ', $post_title_divided_to_lines[0] );
 			$heading_line_2 = implode( ' ', $post_title_divided_to_lines[1] );
-			// echo "<pre>"; print_r($heading_line_1); echo "</pre>";
-			// echo "<pre>"; print_r($heading_line_2); echo "</pre>";
 
 		} else {
 			$heading_line_1 = $post_title;
 		}
 
 
+		// Default args for this class, using some values set above.
 		$this->defaults = [
 			'heading_line_1' => $heading_line_1,
 			'heading_line_2' => $heading_line_2,
       'header_img' => false,
-      'use_homepage_template' => false,
       'description' => false,
       'button_label' => false,
       'button_link' => false,
 			'background_image_url' => false,
 			'accent_image_1_html' => $accent_image_1_html_default,
-			'accent_image_2_html' => $accent_image_2_html_default,
 			'classes' => [
 				'lazyload',
 				'hero',
@@ -94,7 +91,6 @@ class Hero {
     $this->context['button_link'] = $button_link;
 		$this->context['background_image_url'] = $background_image_url;
 		$this->context['accent_image_1_html'] = $accent_image_1_html;
-		$this->context['accent_image_2_html'] = $accent_image_2_html;
 		$this->context['classes'] = implode(' ', $classes);
 	}
 
