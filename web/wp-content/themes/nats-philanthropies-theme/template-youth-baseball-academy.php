cc77de3597ba82bfdf6a68ce7a2f35cf6ec492bc
James Pfleiderer <jpfleiderer@gmail.com>
2020-05-04 14:27:49 -0400

    add post grid to youth baseball template

--- web/wp-content/themes/nats-philanthropies-theme/template-youth-baseball-academy.php
+++ web/wp-content/themes/nats-philanthropies-theme/template-youth-baseball-academy.php
@@ -1,7 +1,8 @@
 <?php
 // Template Name: Youth Baseball Academy Template
-
 use Modules\Hero;
+use Modules\GridCard;
+use Modules\PostGrid;
 
 
 
@@ -34,6 +35,42 @@ $hero->render();
 
 	</div><!-- END .l-content-inner -->
 </div><!-- END .l-content -->
+
+
+
+<?php
+// Create a GridCard for each grid item, store the compiled HTML in the cards array. The array of compiled HTML cards $grid_cards is what is fed to the postGrid module.
+
+$grid_items = get_field( 'card_grid' );
+$grid_cards = [];
+
+foreach( $grid_items as $grid_item ){
+
+	$card_args = [
+		'image_or_icon' => $grid_item['image_or_icon'],
+		'image' => $grid_item['background_image'],
+		'icon' => $grid_item['icon']['icon_selector'],
+		'title' => $grid_item['heading'],
+		'permalink' => $grid_item['link_to'],
+		'excerpt' => $grid_item['excerpt'],
+		'classes' => [ 'postGridItem', 'postGridItem-youthBaseball'],
+	];
+	$new_card = new GridCard($card_args);
+	$grid_cards[] = $new_card->compile();
+
+}
+
+$args = [
+	'grid_items' => $grid_cards,
+	'classes' => [
+		'l-module',
+		'postGrid',
+		'postGrid-youthBaseball',
+	],
+];
+$postGrid = new PostGrid($args);
+$postGrid->render();
+?>
 
 
 
