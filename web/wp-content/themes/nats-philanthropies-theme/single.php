e7b94ff1c2b010d1aebbe7b74507f859c90da1e2
James Pfleiderer <jpfleiderer@gmail.com>
2020-05-05 16:18:05 -0400

    add sharing links to single

--- web/wp-content/themes/nats-philanthropies-theme/single.php
+++ web/wp-content/themes/nats-philanthropies-theme/single.php
@@ -3,7 +3,7 @@ use Modules\Hero;
 
 
 ### Critical CSS for the default single template
-// taoti_enqueue_critical_css( get_template_directory().'/styles/css/critical/single-critical.min.css' );
+taoti_enqueue_critical_css( get_template_directory().'/styles/css/critical/single-critical.min.css' );
 
 
 get_header();
@@ -24,6 +24,34 @@ $hero->render();
 
 <div class="l-content content-single">
 	<div class="l-content-inner">
+
+		<ul class="sharing">
+
+			<li class="sharing-item sharing-item-facebook">
+				<a href="https://www.facebook.com/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>" class="sharing-link sharing-link-facebook" title="Share this page on Facebook" target="_blank" rel="noopener noreferrer">
+					<i class="sharing-icon sharing-icon-facebook"><?php echo file_get_contents( get_template_directory().'/images/social-facebook.svg' ); ?></i>
+				</a>
+			</li>
+
+			<li class="sharing-item sharing-item-twitter">
+				<a href="https://twitter.com/share?url=<?php echo urlencode( get_permalink() ); ?>&text=<?php echo urlencode( 'Check out Washington Nationals Philanthropies! ' ); ?>" class="sharing-link sharing-link-twitter" title="Share this page on Twitter" target="_blank" rel="noopener noreferrer">
+					<i class="sharing-icon sharing-icon-twitter"><?php echo file_get_contents( get_template_directory().'/images/social-twitter.svg' ); ?></i>
+				</a>
+			</li>
+
+			<li class="sharing-item sharing-item-linkedin">
+				<a href="https://www.linkedin.com/shareArticle?url=<?php echo urlencode( get_permalink() ); ?>&title=<?php echo urlencode( get_the_title() ); ?>" class="sharing-link sharing-link-linkedin" title="Share this page on LinkedIn" target="_blank" rel="noopener noreferrer">
+					<i class="sharing-icon sharing-icon-linkedin"><?php echo file_get_contents( get_template_directory().'/images/social-linkedin.svg' ); ?></i>
+				</a>
+			</li>
+
+			<li class="sharing-item sharing-item-email">
+				<a href="mailto:+?subject=<?php echo urlencode( 'Sharing a link from Washington Nationals Philanthropies' ); ?>&body=<?php echo urlencode( 'Hi! I thought you would like to see this from Washington Nationals Philanthropies: "' . get_the_title() . '" (' . get_permalink() ) . ')'; ?>" class="sharing-link sharing-link-email" title="Share this page via email" target="_blank" rel="noopener noreferrer">
+					<i class="sharing-icon sharing-icon-email"><?php echo file_get_contents( get_template_directory().'/images/icon-email.svg' ); ?></i>
+				</a>
+			</li>
+
+		</ul>
 
 		<?php the_page_builder(); ?>
 
