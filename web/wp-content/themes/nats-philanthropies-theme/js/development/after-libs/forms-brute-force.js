e67b7d2b5a3903a4e281d87b2df59e4a45067537
James Pfleiderer <jpfleiderer@gmail.com>
2020-06-23 16:25:00 -0400

    attempting to brute force checked checkboxes

---
+++ web/wp-content/themes/nats-philanthropies-theme/js/development/after-libs/forms-brute-force.js
@@ -0,0 +1,14 @@
+// jQuery('label').click( function(e){
+// 	console.log( jQuery(this).attr('for') );
+// 	var forAtt = jQuery(this).attr('for');
+// 	if( forAtt ){
+// 		var checkbox = document.getElementById(forAtt);checkbox.checked = true;//!checkbox.checked;
+// 		console.log(checkbox);
+// 		console.log(checkbox.checked);
+// 		if( checkbox ){
+// 			checkbox.checked = true;
+// 		}
+// 		jQuery(checkbox).prop("checked", true);
+
+// 	}
+// });
