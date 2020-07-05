fee78e8270281a58ac4fe327270c4bc4dcaf8984
James Pfleiderer <jpfleiderer@gmail.com>
2020-07-05 13:37:35 -0400

    update recommended plugins

--- web/wp-content/themes/jp-base/_recommended-plugins/wordpress-seo/vendor/autoload.php
+++ web/wp-content/themes/jp-base/_recommended-plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5501a8bf1f868cbc0d56aab5ab6c3af6::getLoader();
+return ComposerAutoloaderInit5f6b4390fe0b9f0860b977d3dd766580::getLoader();
--- web/wp-content/themes/jp-base/_recommended-plugins/wp-mail-smtp/uninstall.php
+++ web/wp-content/themes/jp-base/_recommended-plugins/wp-mail-smtp/uninstall.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Uninstalls WP Mail SMTP.
+ * Uninstall all WP Mail SMTP data.
  *
  * @since 1.3.0
  */
@@ -10,20 +10,20 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
+// Load plugin file.
+require_once 'wp_mail_smtp.php';
+
 // Confirm user has decided to remove all data, otherwise stop.
 $settings = get_option( 'wp_mail_smtp', array() );
 if ( empty( $settings['general']['uninstall'] ) ) {
 	return;
 }
 
-// Remove options.
+/*
+ * Remove Legacy options.
+ */
 $options = array(
-	'wp_mail_smtp_initial_version',
-	'wp_mail_smtp_version',
-	'wp_mail_smtp_debug',
-	'wp_mail_smtp',
 	'_amn_smtp_last_checked',
-	// Legacy options.
 	'pepipost_ssl',
 	'pepipost_port',
 	'pepipost_pass',
@@ -38,15 +38,30 @@ $options = array(
 	'mailer',
 	'mail_from_name',
 	'mail_from',
-	'wp_mail_smtp_am_notifications_hidden',
 );
 
 foreach ( $options as $option ) {
 	delete_option( $option );
 }
 
-// Remove product announcements.
-$annoucements = get_posts(
+global $wpdb;
+
+// Delete plugin settings.
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE 'wp\_mail\_smtp%'" );
+
+// Delete plugin user meta.
+$wpdb->query( "DELETE FROM {$wpdb->usermeta} WHERE meta_key LIKE 'wp\_mail\_smtp\_%'" );
+
+// Remove any transients we've left behind.
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '\_transient\_wp\_mail\_smtp\_%'" );
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '\_site\_transient\_wp\_mail\_smtp\_%'" );
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '\_transient\_timeout\_wp\_mail\_smtp\_%'" );
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '\_site\_transient\_timeout\_wp\_mail\_smtp\_%'" );
+
+/*
+ * Remove product announcements.
+ */
+$announcements = get_posts(
 	array(
 		'post_type'   => array( 'amn_smtp' ),
 		'post_status' => 'any',
@@ -54,8 +69,31 @@ $annoucements = get_posts(
 		'fields'      => 'ids',
 	)
 );
-if ( ! empty( $annoucements ) ) {
-	foreach ( $annoucements as $annoucement ) {
-		wp_delete_post( $annoucement, true );
+if ( ! empty( $announcements ) ) {
+	foreach ( $announcements as $announcement ) {
+		wp_delete_post( $announcement, true );
 	}
 }
+
+/*
+ * Logs for Pro plugin only.
+ */
+if (
+	function_exists( 'wp_mail_smtp' ) &&
+	is_readable( wp_mail_smtp()->plugin_path . '/src/Pro/Pro.php' )
+) {
+	// DB table.
+	$logs_table = \WPMailSMTP\Pro\Emails\Logs\Logs::get_table_name();
+	$wpdb->query( "DROP TABLE IF EXISTS $logs_table;" ); // phpcs:ignore WordPress.DB
+}
+
+/*
+ * Drop all Action Scheduler data.
+ */
+require_once dirname( __FILE__ ) . '/vendor/woocommerce/action-scheduler/action-scheduler.php';
+
+// Unschedule all plugin ActionScheduler actions.
+( new \WPMailSMTP\Tasks\Tasks() )->cancel_all();
+
+$meta_table = \WPMailSMTP\Tasks\Meta::get_table_name();
+$wpdb->query( "DROP TABLE IF EXISTS $meta_table;" ); // phpcs:ignore WordPress.DB
