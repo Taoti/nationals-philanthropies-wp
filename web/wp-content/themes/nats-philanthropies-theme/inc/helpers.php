d8a3f0068cc660266cbdf338808aab1016ff386e
James Pfleiderer <jpfleiderer@gmail.com>
2021-02-15 16:19:48 -0500

    Prevent excerpts from using the page builder content as a backup

--- web/wp-content/themes/nats-philanthropies-theme/inc/helpers.php
+++ web/wp-content/themes/nats-philanthropies-theme/inc/helpers.php
@@ -4,6 +4,21 @@
 // These functions help other areas in the code process or retrieve something. Most likely, helper functions will have a return value.
 
 
+
+### Set which post types are using the page builder. This helps generate excerpts when the_content() isn't used, or is used in conjunction with the_page_builder().
+function taoti_get_post_types_with_page_builder(){
+
+	$post_types_with_page_builder = [
+		'post',
+		'page',
+		'events'
+	];
+
+	return $post_types_with_page_builder;
+}
+
+
+
 ### Give this a string and a integer length and it will return a string that is cut to that length of characters. If the string was indeed shortened it will add ... (ellipsis) to the end.
 function jp_substr( $string, $length=50, $ellipsis = true ){
     $excerpt = wp_strip_all_tags($string, true);
