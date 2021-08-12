b94b0a0d23c32ade76e67b3d8e5460306a4f0b90
Mariano Labollita <marianolabollita@MacBook-Air-de-Mariano.local>
2021-08-12 13:15:28 -0300

    Added background color selection to homepage Image module

--- web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/homePage_image.php
+++ web/wp-content/themes/nats-philanthropies-theme/flex-content/layouts/homePage_image.php
@@ -3,6 +3,8 @@ use Modules\ContentGroup;
 use JP\Get;
 ?>
 <section class="l-homePageModule homeImage scrollspy scrollspy-dark">
+  <?php $bg_home_color = get_sub_field('bg_home_color'); ?>
+  <div class="homeImageBg" style="background-color: <?php echo ($bg_home_color) ? $bg_home_color : '#AB0003' ?>;"></div>
   <div class="homeImage-inner">
 
       <div class="homeImage-imageContainer">
@@ -27,13 +29,13 @@ use JP\Get;
         <?php
         // ContentGroup
         $args = [
-         'primary_heading' => get_sub_field('primary_heading_line_1'),
-         'secondary_heading' => get_sub_field('primary_heading_line_2'),
-         'tertiary_heading' => get_sub_field('primary_heading_line_3'),
-         'description' => get_sub_field('description'),
-         'cta_link' => get_sub_field('button_url'),
-				 'cta_label' => get_sub_field('button_label'),
-				 'use_inverted_accent' => true,
+        'primary_heading' => get_sub_field('primary_heading_line_1'),
+        'secondary_heading' => get_sub_field('primary_heading_line_2'),
+        'tertiary_heading' => get_sub_field('primary_heading_line_3'),
+        'description' => get_sub_field('description'),
+        'cta_link' => get_sub_field('button_url'),
+        'cta_label' => get_sub_field('button_label'),
+        'use_inverted_accent' => true,
         ];
         $contentGroup = new ContentGroup($args);
         $contentGroup->render();
