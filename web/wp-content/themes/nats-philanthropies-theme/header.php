babb39975574988ac63018be7ede0ac0031b5823
sebastian <sebastian@wupplier.com>
2021-12-02 17:01:15 -0500

    Replacing wrong chars on GTM code

--- web/wp-content/themes/nats-philanthropies-theme/header.php
+++ web/wp-content/themes/nats-philanthropies-theme/header.php
@@ -3,13 +3,11 @@
 
 <head>
     <!-- Google Tag Manager -->
-
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
-      new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
-      ’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,‘script’,‘dataLayer’,‘GTM-K6G6V4T’);</script>
-
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-K6G6V4T');</script>
     <!-- End Google Tag Manager -->
 
   <?php
@@ -60,9 +58,7 @@ if( $temporary_page_enabled ){
 ?>
 <body <?php body_class( $extra_body_classes ); ?>>
   <!-- Google Tag Manager (noscript) -->
-
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6G6V4T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6G6V4T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 
   <?php do_action('jp_body_start'); ?>
